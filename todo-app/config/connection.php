e80e6a6c5e64d0d9161f8dedfad67d1634a30d89
Matías Ramos <matiasramos261@gmail.com>
2021-03-27 15:34:20 -0300

    Update connection.php

--- todo-app/config/connection.php
+++ todo-app/config/connection.php
@@ -3,7 +3,7 @@
 class Database{
 
     public static function connect(){
-        $db = new mysqli('localhost', 'matias0303456', 'positivo2MaTiAs', 'todo_app');
+        $db = new mysqli('localhost', 'root', '', 'todo_app');
         $db->query('set names utf8');
 
         return $db;
